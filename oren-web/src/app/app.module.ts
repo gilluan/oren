b67cecc2c00d0273caa1f46fc5a014cc11c3a7f4
Gilluan Formiga <gilluan@hotmail.com.br>
2017-11-23 23:13:22 -0200

    adicionando rotas

--- oren-web/src/app/app.module.ts
+++ oren-web/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { appRoutes } from './routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -8,6 +10,9 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentHeaderComponent } from './content-header/content-header.component';
 import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +25,11 @@ import { ControlSidebarComponent } from './control-sidebar/control-sidebar.compo
     ControlSidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
