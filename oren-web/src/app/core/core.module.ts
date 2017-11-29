3d1abc5b112923366c17541b5c85415f63564348
Gilluan Formiga <gilluan@hotmail.com.br>
2017-11-29 00:57:06 -0200

    add course service

---
+++ oren-web/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { CourseService } from './../functionalities/course.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  providers: [CourseService]
+})
+export class CoreModule { }
